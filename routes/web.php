e67e98f47148c764e1e872353c83d04e706f4c11
yaya1602 <alya24si@mahasiswa.pcr.ac.id>
2025-10-02 08:02:39 +0700

    Nambah spasi di web.php

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
 
 Route::get('/about', function () {
     return view('halaman-about');
-});
+}
+
 
 Route::get('/home',[HomeController::class,'index']);
