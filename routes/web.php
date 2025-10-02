6d4f0417cc55e91e5965206d1dfc017c0266e789
yaya1602 <alya24si@mahasiswa.pcr.ac.id>
2025-10-02 07:47:26 +0700

    Pertemuan 4

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
     return view('welcome');
 
-    
+
 });
 Route::get('/pcr', function () {
     return 'Selamat Datang di Website Kampus PCR!';
@@ -25,3 +26,5 @@ Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
 Route::get('/about', function () {
     return view('halaman-about');
 });
+
+Route::get('/home',[HomeController::class,'index']);
