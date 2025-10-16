347d772fb7222545187e544ef6a9dc45c71a3b4b
yaya1602 <alya24si@mahasiswa.pcr.ac.id>
2025-10-16 11:02:35 +0700

    halaman edit data pelanggan

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\Pelanggan;
@@ -13,7 +12,7 @@ class PelangganController extends Controller
     public function index()
     {
         $data['dataPelanggan'] = Pelanggan::all();
-		return view('admin.pelanggan.index',$data);
+        return view('admin.pelanggan.index', $data);
     }
 
     /**
@@ -31,17 +30,16 @@ class PelangganController extends Controller
     {
         //dd($request->all());
 
-		$data['first_name'] = $request->first_name;
-		$data['last_name'] = $request->last_name;
-		$data['birthday'] = $request->birthday;
-		$data['gender'] = $request->gender;
-		$data['email'] = $request->email;
-		$data['phone'] = $request->phone;
+        $data['first_name'] = $request->first_name;
+        $data['last_name']  = $request->last_name;
+        $data['birthday']   = $request->birthday;
+        $data['gender']     = $request->gender;
+        $data['email']      = $request->email;
+        $data['phone']      = $request->phone;
 
-		Pelanggan::create($data);
+        Pelanggan::create($data);
 
-
-		return redirect()->route('pelanggan.index')->with('success','Penambahan Data Berhasil!');
+        return redirect()->route('pelanggan.index')->with('success', 'Penambahan Data Berhasil!');
     }
 
     /**
@@ -57,7 +55,8 @@ class PelangganController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $data['dataPelanggan'] = Pelanggan::findOrFail($id);
+        return view('admin.pelanggan.edit', $data);
     }
 
     /**
@@ -65,7 +64,19 @@ class PelangganController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $pelanggan_id =$id;
+        $pelanggan = Pelanggan::findOrFail($pelanggan_id);
+
+        $pelanggan->first_name =$request->first_name;
+        $pelanggan->last_name =$request->last_name;
+        $pelanggan->birthday =$request->birthday;
+        $pelanggan->gender=$request->gender;
+        $pelanggan->email =$request->email;
+        $pelanggan->phone =$request->phone;
+
+        $pelanggan->save();
+        return redirect()->route('pelanggan.index')->with('succes','Perubahan Data Berhasil!');
+
     }
 
     /**
--- resources/views/admin/pelanggan/edit.blade.php
+++ resources/views/admin/pelanggan/edit.blade.php
@@ -116,7 +116,7 @@
                 </li>
                 <!-- Tambahkan blok kode ini -->
                 <li class="nav-item  active ">
-                    <a href="" class="nav-link">
+                    <a href="{{ route('pelanggan.index') }}" class="nav-link">
                         <span class="sidebar-icon">
                             <svg class="icon icon-xs me-2" data-slot="icon" fill="none" stroke-width="1.5"
                                 stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
@@ -129,7 +129,6 @@
                         <span class="sidebar-text">Pelanggan</span>
                     </a>
                 </li>
-
                 <li role="separator" class="dropdown-divider mt-4 mb-3 border-gray-700"></li>
                 <li class="nav-item">
                     <a href="https://themesberg.com/docs/volt-bootstrap-5-dashboard/getting-started/quick-start/"
@@ -345,76 +344,118 @@
                         </a>
                     </li>
                     <li class="breadcrumb-item"><a href="#">Pelanggan</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Edit Data</li>
                 </ol>
             </nav>
             <div class="d-flex justify-content-between w-100 flex-wrap">
                 <div class="mb-3 mb-lg-0">
-                    <h1 class="h4">Data Pelanggan</h1>
-                    <p class="mb-0">List data seluruh pelanggan</p>
+                    <h1 class="h4">Edit Pelanggan</h1>
+                    <p class="mb-0">Form untuk edit data pelanggan.</p>
                 </div>
                 <div>
-                    <a href="{{ route('pelanggan.create') }}" class="btn btn-success text-white"><i
-                            class="far fa-question-circle me-1"></i> Tambah Pelanggan</a>
+                    <a href="{{ route('pelanggan.index') }}" class="btn btn-primary"><i
+                            class="far fa-question-circle me-1"></i> Kembali</a>
                 </div>
             </div>
         </div>
-        {{-- info tambah data --}}
-        @if (session('create'))
-            <div class="alert alert-info">
-                {!! session('create') !!}
-            </div>
-        @endif
-        {{-- info edit data --}}
-        @if (session('update'))
-            <div class="alert alert-info">
-                {!! session('update') !!}
-            </div>
-        @endif
-        
+
         <div class="row">
             <div class="col-12 mb-4">
-                <div class="card border-0 shadow mb-4">
+                <div class="card border-0 shadow components-section">
                     <div class="card-body">
-                        <div class="table-responsive">
-                            <table id="table-pelanggan" class="table table-centered table-nowrap mb-0 rounded">
-                                <thead class="thead-light">
-                                    <tr>
-                                        <th class="border-0">First Name</th>
-                                        <th class="border-0">Last Name</th>
-                                        <th class="border-0">Birthday</th>
-                                        <th class="border-0">Gender</th>
-                                        <th class="border-0">Email</th>
-                                        <th class="border-0">Phone</th>
-                                        <th class="border-0 rounded-end">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($dataPelanggan as $item)
-                                        <tr>
-                                            <td>{{ $item->first_name }}</td>
-                                            <td>{{ $item->last_name }}</td>
-                                            <td>{{ $item->birthday }}</td>
-                                            <td>{{ $item->gender }}</td>
-                                            <td>{{ $item->email }}</td>
-                                            <td>{{ $item->phone }}</td>
-                                            <td><a href="{{ route('pelanggan.edit', $item->pelanggan_id) }}"
-                                                    class="btn btn-info btn-sm">
+                        <form action="{{ route('pelanggan.update', $dataPelanggan->pelanggan_id) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="row mb-4">
+                                <div class="col-lg-4 col-sm-6">
+                                    <!-- First Name -->
+                                    <div class="mb-3">
+                                        <label for="first_name" class="form-label">First name</label>
+                                        <input type="text" name="first_name"id="first_name" class="form-control"
+                                            required value="{{ $dataPelanggan->first_name }}">
+                                    </div>
+
+                                    <!-- Last Name -->
+                                    <div class="mb-3">
+                                        <label for="last_name" class="form-label">Last name</label>
+                                        <input type="text" name="last_name" id="last_name" class="form-control"
+                                            required value="{{ $dataPelanggan->last_name }}">
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-4 col-sm-6">
+                                    <!-- Birthday -->
+                                    <div class="mb-3">
+                                        <label for="birthday" class="form-label">Birthday</label>
+                                        <input type="date" name="birthday" id="birthday" class="form-control"
+                                            value="{{ $dataPelanggan->birthday }}">
+                                    </div>
+
+                                    <!-- Gender -->
+                                    <div class="mb-3">
+                                        <label for="gender" class="form-label">Gender</label>
+                                        <select id="gender" name="gender" class="form-select">
+                                            <option selected>Gender</option>
+                                            <option value="Female"
+                                                {{ $dataPelanggan->gender == 'Female' ? 'selected' : '' }}>Female
+                                            </option>
+                                            <option value="Male"
+                                                {{ $dataPelanggan->gender == 'Male' ? 'selected' : '' }}>Male</option>
+
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-4 col-sm-12">
+                                    <!-- Email -->
+                                    <div class="mb-3">
+                                        <label for="email" class="form-label">Email</label>
+                                        <input type="text" name="email" id="email" class="form-control"
+                                            required value="{{ $dataPelanggan->email }}">
+                                    </div>
+
+                                    <!-- Phone -->
+                                    <div class="mb-3">
+                                        <label for="phone" class="form-label">Phone</label>
+                                        <input type="text" name=" phone" id="phone" class="form-control"
+                                            value="{{ $dataPelanggan->phone }}">
+                                    </div>
+
+                                    <!-- Buttons -->
+                                    <div class="">
+                                        <button type="submit" class="btn btn-primary">Simpan</button>
+                                        <a href="{{ route('pelanggan.index') }}"
+                                            class="btn btn-outline-secondary ms-2">Batal</a>
+                                        <li class="nav-item  active ">
+                                            <a href="{{ route('dashboard') }}" class="nav-link">
+
+                                                <span class="sidebar-text">Dashboard</span>
+                                            </a>
+                                        </li>
+
+                                        <!-- Tambahkan blok kode ini -->
+                                        <li class="nav-item  active ">
+                                            <a href="" class="nav-link">
+                                                <span class="sidebar-icon">
                                                     <svg class="icon icon-xs me-2" data-slot="icon" fill="none"
                                                         stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24"
                                                         xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                                         <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10">
+                                                            d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z">
                                                         </path>
                                                     </svg>
-                                                    Edit
-                                                </a></td>
-                                        </tr>
-                                    @endforeach
+                                                </span>
+                                                <span class="sidebar-text">Pelanggan</span>
+                                            </a>
+                                        </li>
 
-                                </tbody>
-                            </table>
-                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
                     </div>
+
                 </div>
             </div>
         </div>
